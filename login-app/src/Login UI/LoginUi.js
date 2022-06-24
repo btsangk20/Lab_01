f01df3b1b40287274158c29c143aff244f655295
Tấn Sang <btsang20@clc.fitus.edu.vn>
2022-06-24 13:52:09 +0700

    create login page UI

--- login-app/src/Login UI/LoginUi.js
+++ login-app/src/Login UI/LoginUi.js
@@ -1,10 +1,37 @@
 import './LoginUi.css';
-import profile from "./../image/a.png";
+import profile from "./../image/profile.png";
 import email from "./../image/email.jpg";
 import pass from "./../image/pass.png";
 function LoginUi() {
   return (
-    <div></div>
+    <div className="login-page">
+     <div className="login-container">
+       <div>
+         <div className="login-imgs">
+           <div className="container-image">
+              <img src={profile} alt="profile" className="login-profile-image"/>
+           </div>
+         </div>
+        <div>
+           <h1>Login</h1>
+           <div>
+              <img src={email} alt="email" className="login_email-image"/>
+              <input type="text" placeholder="Enter email" className="login_email-input"/>
+           </div>
+           <div className="second-input">
+              <img src={pass} alt="pass" className="login_password-image"/>
+              <input type="password" placeholder="Enter password" className="login_password-input"/>
+           </div>
+        <div className="login-button">
+          <button>Login</button>
+          </div>
+            <p className="link">
+              <a href="#">Forgot password?</a> Or <a href="#">Create a new account</a>
+            </p>
+          </div>
+        </div>
+     </div>
+    </div>
   );
 }
 export default LoginUi;
