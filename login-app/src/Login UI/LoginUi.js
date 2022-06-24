f640bb4c9a132d284ea48fbf7afc4d18610d0686
Tấn Sang <btsang20@clc.fitus.edu.vn>
2022-06-24 13:04:24 +0700

    add image and edit file

---
+++ login-app/src/Login UI/LoginUi.js
@@ -0,0 +1,10 @@
+import './LoginUi.css';
+import profile from "./../image/a.png";
+import email from "./../image/email.jpg";
+import pass from "./../image/pass.png";
+function LoginUi() {
+  return (
+    <div></div>
+  );
+}
+export default LoginUi;
